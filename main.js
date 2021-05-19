3c5f923730e0c8d6af290be24f91e76bc2b507d7
Namrutha Hari <clickfornamz@gmail.com>
2021-05-19 14:14:55 +0530

    'added'

--- main.js
+++ main.js
@@ -21,11 +21,11 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImagePath2); //TWO
 
 //If player 1 wins
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
 }
 //If player 2 wins
 else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!";
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
 }
 //If draw
 else {
